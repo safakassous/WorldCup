294466d9aa1585d92115a6e45e3c2013aa596846
unknown <farahcherif7@gmail.com>
2021-05-01 18:41:10 +0200

    paiement billet backend

---
+++ SBackend/src/main/java/com/Qatar2022/sbmail/controller/AttachmentEmailExampleController.java
@@ -0,0 +1,55 @@
+package com.Qatar2022.sbmail.controller;
+ 
+import java.io.File;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.Qatar2022.models.AcheteurTransaction;
+
+
+@CrossOrigin(origins ="http://localhost:4200")
+ @Controller
+public class AttachmentEmailExampleController {
+ 
+    @Autowired
+    public JavaMailSender emailSender;
+
+    @ResponseBody 
+    @PostMapping("/sendAttachmentEmail")
+    public String sendAttachmentEmail(@RequestBody AcheteurTransaction a) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+ 
+        boolean multipart = true;
+ 
+        MimeMessageHelper helper = new MimeMessageHelper(message, multipart);
+ 
+        helper.setTo(a.getMail_acheteur());
+        helper.setSubject("Notification paiement");
+
+        helper.setText(a.getCorps_mail());
+         
+        String path1 = "C:\\Users\\Farah\\OneDrive\\Bureau\\TP1\\hihi.txt";
+       // String path2 = "/home/tran/Downloads/readme.zip";
+ 
+        // Attachment 1
+        FileSystemResource file1 = new FileSystemResource(new File(path1));
+        helper.addAttachment("Facture", file1);
+ 
+        // Attachment 2
+        //  FileSystemResource file2 = new FileSystemResource(new File(path2));
+        //   helper.addAttachment("Readme", file2);
+ 
+        emailSender.send(message);
+ 
+        return "Email Sent!";
+    }
+ 
+}
